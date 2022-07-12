4b3c831ee36d238be214180695567d15ab8e95af
Jon Veach <ozzy_ozbourn@hotmail.com>
2022-07-12 16:16:52 -0400

    Updated link

--- commands/postoffice.js
+++ commands/postoffice.js
@@ -11,7 +11,7 @@ module.exports = {
 			.setTitle('DomiNATION Post Office & Public Workshops')
 			.setURL('https://docs.google.com/spreadsheets/d/1G60kchCLZtvi6oRyQnDnH5EoEwuvVWc5G-33zGc0x-4/edit?usp=sharing')
 			.setColor('#3E82F7')
-			.setDescription("G'day Survivor!  Here's a list of Post Offices and Public Workshops here on Domi!  https://docs.google.com/spreadsheets/d/1JBpMMLBX8QABlMcF3VC-nCnsERQIcCyHy8E3QFb2NM0/edit#gid=0")
+			.setDescription("G'day Survivor!  Here's a list of Post Offices and Public Workshops here on Domi!  https://docs.google.com/spreadsheets/d/1G60kchCLZtvi6oRyQnDnH5EoEwuvVWc5G-33zGc0x-4/edit?usp=sharing")
 		await interaction.reply({ embeds: [embed], ephemeral: false });
 	},
 };
