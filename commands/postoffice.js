3fec918b0c875e14ed62ea87d9cce2b3ed9eaa92
Jon Veach <ozzy_ozbourn@hotmail.com>
2022-07-12 16:09:10 -0400

    Updated link

--- commands/postoffice.js
+++ commands/postoffice.js
@@ -9,7 +9,7 @@ module.exports = {
 	async execute(interaction, bot) {
 		const embed = new MessageEmbed()
 			.setTitle('DomiNATION Post Office & Public Workshops')
-			.setURL('https://docs.google.com/spreadsheets/d/1JBpMMLBX8QABlMcF3VC-nCnsERQIcCyHy8E3QFb2NM0/edit#gid=0')
+			.setURL('https://docs.google.com/spreadsheets/d/1G60kchCLZtvi6oRyQnDnH5EoEwuvVWc5G-33zGc0x-4/edit?usp=sharing')
 			.setColor('#3E82F7')
 			.setDescription("G'day Survivor!  Here's a list of Post Offices and Public Workshops here on Domi!  https://docs.google.com/spreadsheets/d/1JBpMMLBX8QABlMcF3VC-nCnsERQIcCyHy8E3QFb2NM0/edit#gid=0")
 		await interaction.reply({ embeds: [embed], ephemeral: false });
